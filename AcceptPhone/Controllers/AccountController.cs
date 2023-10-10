b72104fe307893502a294609d9edf0a4d3b9eed5
Juan David Saavedra Reyes <juansaavedra2406@gmail.com>
2023-10-09 20:19:18 -0500

    first commit

---
+++ AcceptPhone/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using AcceptPhone.Models;
+using System.Data.SqlClient;
+using System.Web.Mvc;
+using System.Windows.Forms;
+
+namespace AcceptPhone.Controllers
+{
+
+    public class AccountController : Controller
+    {
+        // Acción para mostrar el formulario de registro
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // Acción para procesar el formulario de registro
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            // Aquí puedes agregar la lógica de registro, como guardar el usuario en la base de datos
+            // y redirigirlo a una página de inicio de sesión o confirmación.
+            bool result = false;
+
+            string cs = "DataSource = localhost; InitialCatalog = Accephone";
+
+            string SQL = "INSERT INTO Usuarios (IdUsuario, Correo , Clave )"
+                + "VALUES (" + usuario.Id_usuario + "," + "juan@gmail.com" + " , " + usuario.clave + " );";
+
+            SqlConnection cone = new SqlConnection(cs);
+
+
+            cone.Open();
+            SqlCommand command = new SqlCommand(SQL, cone);
+            SqlDataReader reader = command.ExecuteReader();
+            MessageBox.Show(reader.Read().ToString());
+
+
+            //if (reader.Read() == true)
+            //{
+            //    result = true;
+            //}
+            //else
+            //{
+            //    result = false;
+            //}
+
+
+            // return RedirectToAction("Login", "Account"); // Redirige al usuario a la página de inicio de sesión
+        }
+
+        // Otras acciones, como Login, Logout, etc., pueden ir aquí
+    }
+
+}
---
+++ AcceptPhone/Models/RegisterViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+
+namespace AcceptPhone.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Nombre de usuario")]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Correo electrónico")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar contraseña")]
+        [Compare("Password", ErrorMessage = "Las contraseñas no coinciden.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
